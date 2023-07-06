c449bf2ed991edaccff7897066035b0943d446ae
surajit <bikymaity489@gmail.com>
2023-07-06 23:53:12 +0530

    array input

---
+++ Array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+#include "superscript.h"
+using namespace std;
+
+
+
+int main() {
+  int n;
+
+  cout << "Enter length how many number to work with;"<< endl;
+
+  cin >> n;
+    int numbers[n];
+  cout << "ok, now enter all " << n << " number one by one" << endl;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j < i + 1; j++) {
+      cout<< "[" << numbers[j - 1] << "]"<<getSuperscriptForm(j-1)<<" ";
+    }
+    cout<< "[..";
+    cin >> numbers[i];
+
+    if (i < (n - 1)) clrscr();
+
+  }
+clrscr();
+  cout<<"Here is your numbers"<<endl;
+  cout<<"[ ";
+  for(int i=0;i<n;i++){
+    cout<<numbers[i];
+    if(i<n-1) cout<<" , ";
+  }
+   cout<<"]";
+  return 0;
+}
--- nCr.cpp
+++ nCr.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 
 int main(){
-     int n,c;
+    int n,c;
     cout<<"Enter n"<<endl;
     cin>>n;
     cout<<"Enter c"<<endl;
     cin>>c;
     cout<<facto(n)/(facto(n-c)*facto(c))<<endl;
-return 0;
+ return 0;
 }
