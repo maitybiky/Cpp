6093a8da26fcfb1f7a21d10ac019ddd8cd4383b0
surajit <bikymaity489@gmail.com>
2023-08-04 11:45:29 +0530

    pointer as argument

--- Pointer/index.cpp
+++ Pointer/index.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    int num =10;
-    int *ptr=&num;
+void swap(int *x,int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
 
-    cout<<&num<<endl; // adress hexadecimal
-    cout<<ptr<<endl;  // adress  hexadecimal
-    cout<<*ptr<<endl; // valure of num
-    return 0;
+void swap(int x,int y){
+    int temp=x;
+    x=y;
+    y=temp;
+}
+
+
+int main() {
+  int num = 10;
+  int * ptr = & num;
+
+  cout << & num << endl; // adress hexadecimal
+  cout << ptr << endl; // adress  hexadecimal
+  cout << * ptr << endl; // valure of num
+
+  ptr++;
+  cout << ptr << endl; // next memory adress  hexadecimal
+  cout << * ptr << endl; // garbage value
+
+  // Pointers and Arrays
+
+  int arr[] = {
+    3,
+    4,
+    6
+  };
+  cout << "*array" << * arr << endl; // arr is a constant pointer to the first element of the array
+  int * arrPtr = arr; // arrPtr is a pointer to the first element of the array
+  for (int i = 0; i < 3; i++) {
+    cout << * arrPtr << endl;
+    arrPtr++;
+  }
+
+
+
+
+// pointer as argument
+
+int a=3;
+int b=5;
+swap(&a,&b); // change original value
+cout<<"a "<<a<<" b "<<b<<endl;
+swap(a,b);   // function overloading : does not change original value
+cout<<"a "<<a<<" b "<<b<<endl;
+  return 0;
 }
