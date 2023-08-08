16af06794fa495256ca4f7c6e9021e0dd8da612d
surajit <bikymaity489@gmail.com>
2023-08-08 14:22:28 +0530

    x raised to power n modified

--- recursion/xRaisedToThePowerN.cpp
+++ recursion/xRaisedToThePowerN.cpp
@@ -4,21 +4,20 @@ using namespace std;
 
 float xpn(float x, float n) {
   if (n < 0) {
-   
-    if (n == -1) return 1 / x;
-    return xpn(x+x, n + 1);
-  } 
+    if (n == 0) return 1 ;
+    return ((1/x) * xpn(x, n + 1));
+  }
 
-    if (n == 1) return  x;
-    return xpn(x+x, n - 1);
+  if (n == 0) return 1;
+  return x * xpn(x, n - 1);
 
 }
 
-int main(){
+int main() {
 
-    float x,n;
-    cin>>x;
-    cin>>n;
-    cout<<xpn(x,n);
-    return 0;
+  float x, n;
+  cin >> x;
+  cin >> n;
+  cout << xpn(x, n);
+  return 0;
 }
