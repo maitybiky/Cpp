bf256b3955e833bbbef9d084ae4d49ae2aaec732
surajit <bikymaity489@gmail.com>
2023-08-10 20:18:31 +0530

    add a space

--- recursion/inc-dec-num.cpp
+++ recursion/inc-dec-num.cpp
@@ -9,6 +9,7 @@ cout<<n<<" ";
  dec(n-1);
 }
 
+
 void inc(int n){
     if(n==0) return;
     inc(n-1);
@@ -19,7 +20,7 @@ void inc(int n){
 int main(){
     int n;
     cin>>n;
-    
+
     dec(n);
     cout<<endl;
     inc(n);
