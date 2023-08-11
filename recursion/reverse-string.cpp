45ac9eceab40929f36361d97130247c448f7b75e
surajit <bikymaity489@gmail.com>
2023-08-11 11:15:45 +0530

    reverse a string using recursion

---
+++ recursion/reverse-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string rev(string& str,int ind=0){
+
+    if(!str[ind]) return "";
+
+     return  rev(str,ind+1)+str[ind];
+}
+
+int main(){
+    string s="hospital";
+    cout<<rev(s);
+    return 0;
+}
