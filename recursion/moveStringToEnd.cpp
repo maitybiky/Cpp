bd32cc311bf62738ca866b0972b2df0ff6aab910
surajit <bikymaity489@gmail.com>
2023-08-12 21:23:02 +0530

    print substring of a string

--- recursion/moveStringToEnd.cpp
+++ recursion/moveStringToEnd.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
+
 #include<string>
+
 using namespace std;
 
-string moveToEnd(string str,int n=1){
- int ind=str.length()-n; //ind 
-if(ind==0) return str;
-if(str[ind]=='x') {
-    string rem=str.erase(ind,1);
-    string pushedX=rem+"x";
-    return  moveToEnd(pushedX,n+1);
-}
-return moveToEnd(str,n+1);
+
+//my code 
+string moveToEnd(string str, int n = 1) {
+  int ind = str.length() - n; //navgating from end of the string 
+
+  if (ind == 0) return str;
+  if (str[ind] == 'x') {
+    string rem = str.erase(ind, 1);
+    string pushedX = rem + "x";
+    return moveToEnd(pushedX, n + 1);
+  }
+  return moveToEnd(str, n + 1);
 }
 
+// youtube
 
-int main(){
-    string p="axxbxxxffdfx";
-   
-    cout<<moveToEnd(p);
-return 0;
+string MoveToEnd(string str){
+    if(str.length()==0) return "";
+    
+     char firstChar=str[0];
+    
+    if(firstChar=='x'){
+        return MoveToEnd(str.substr(1)) + firstChar;
+    }
+    return firstChar+MoveToEnd(str.substr(1)); 
+}
+int main() {
+  string p = "axxbxxxffdfx";
+
+//   cout << moveToEnd(p);
+  cout << MoveToEnd(p);
+  return 0;
 }
---
+++ recursion/substring.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+void subset (string str,string rnode=""){
+    if(str.length()==0) {
+cout<<rnode<<endl;
+return ;
+    } 
+    
+  subset(str.substr(1),rnode+str[0]);
+  subset(str.substr(1),rnode);
+  
+}                                  
+
+int main(){
+
+subset("ABC");
+return 0;
+}
