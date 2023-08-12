32ef49e9f48768c906294ea459b54ec630226909
surajit <bikymaity489@gmail.com>
2023-08-12 18:21:42 +0530

    move all x to end of the string

---
+++ recursion/moveStringToEnd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+string moveToEnd(string str,int n=1){
+ int ind=str.length()-n; //ind 
+if(ind==0) return str;
+if(str[ind]=='x') {
+    string rem=str.erase(ind,1);
+    string pushedX=rem+"x";
+    return  moveToEnd(pushedX,n+1);
+}
+
+return moveToEnd(str,n+1);
+
+}
+
+int main(){
+    string p="axxbxxxffdfx";
+   
+    cout<<moveToEnd(p);
+return 0;
+}
