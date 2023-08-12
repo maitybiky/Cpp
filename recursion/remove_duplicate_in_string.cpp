395df803b2345aed47cb08cb22604dffb3425ddf
surajit <bikymaity489@gmail.com>
2023-08-12 15:21:10 +0530

    All file Visual change

--- recursion/remove_duplicate_in_string.cpp
+++ recursion/remove_duplicate_in_string.cpp
@@ -8,7 +8,7 @@ if(str.length()==n) return str;
     if(str[n+1]==str[n]) return remDuplicate(str.erase(n,1),n);
     return remDuplicate(str,n+1);
 }
-
+ 
 int main(){
 string p="ggurrrriiiizsss";
 cout<<remDuplicate(p)<<endl;
