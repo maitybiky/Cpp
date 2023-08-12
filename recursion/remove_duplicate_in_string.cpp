b10e25e710b009c0bcd85683b54648223e054479
surajit <bikymaity489@gmail.com>
2023-08-12 15:15:43 +0530

    remove duplicate (if only characters next to each)

---
+++ recursion/remove_duplicate_in_string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+string remDuplicate(string str,int n=0){
+
+if(str.length()==n) return str;
+    if(str[n+1]==str[n]) return remDuplicate(str.erase(n,1),n);
+    return remDuplicate(str,n+1);
+}
+
+int main(){
+string p="ggurrrriiiizsss";
+
+cout<<remDuplicate(p)<<endl;
+
+return 0;
+
+}
--- recursion/replace-pi-with-value.cpp
+++ recursion/replace-pi-with-value.cpp
@@ -4,20 +4,20 @@
 
 using namespace std;
 
-string replacePI(char str[]) {
+string replacePI(string str) {
   string PIv = "3.14";
   if (str[0] == '\0') return "";
   if (str[0] == 'p' && str[1] == 'i') {
   
-    return PIv+replacePI(str + 2);
+    return PIv+replacePI(str.substr(2));
   } else {
    
-    return str[0]+replacePI(str + 1);
+    return str[0]+replacePI(str.substr(1));
   }
 
 }
 int main() {
-  char PIstring[] = "pippxxppixipi";
+  string PIstring = "pippxxppixipi";
 cout<<replacePI(PIstring)<<endl;
   
   return 0;
