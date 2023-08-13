20ac43c8cbf5af6df9570a4cdc6f3d8ab13e6206
surajit <bikymaity489@gmail.com>
2023-08-13 14:57:15 +0530

    all combination of two string

--- recursion/possible-letter-combination-in-keypad-phone.cpp
+++ recursion/possible-letter-combination-in-keypad-phone.cpp
@@ -6,23 +6,22 @@ using namespace std;
 
 void combo(string x, string y) {
 
-    if(x.length()==0) return;
+  if (x.length() == 0) return;
 
-     char fchar= x[0];
-     for(int i=0;i<y.length();i++){
-        cout<<fchar<<y[i]<<endl;
-     } 
-     combo(x.substr(1),y);
-
+  char fchar = x[0];
+  for (int i = 0; i < y.length(); i++) {
+    cout << fchar << y[i] << endl;
+  }
+  combo(x.substr(1), y);
 
 }
 
 int main() {
-string keys []={"./","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-int fnum;
-int lnum;
-cin>>fnum;
-cin>>lnum;
-  combo(keys[fnum-1],keys[lnum-1]);
+  string keys []={"./","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+  int fnum;
+  int lnum;
+  cin >> fnum;
+  cin >> lnum;
+  combo(keys[fnum - 1], keys[lnum - 1]);
   return 0;
 }
