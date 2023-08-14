3844fc16f561238c0ec55be14096fcfcd2595b7a
surajit <bikymaity489@gmail.com>
2023-08-14 16:32:24 +0530

    recursion path pending

--- recursion/TowerOfHanoi.cpp
+++ recursion/TowerOfHanoi.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
-void TowerOfH(int s,int e,char src, char aux,char des){
-    if(e<s) return;
-    TowerOfH(s,e-1,src,des,aux);
-cout<<"move "<<e<<" From "<<src<<" to "<<des<<endl;
-TowerOfH(s,e-1,aux,src,des);
+void TowerOfH(int s, int e, char src, char aux, char des) {
+  if (e < s) return;
+  TowerOfH(s, e - 1, src, des, aux);
+  cout << "move " << e << " From " << src << " to " << des << endl;
+  TowerOfH(s, e - 1, aux, src, des);
 }
 
-int main(){
-TowerOfH(1,3,'A','B','C');
-    return 0;
+int main() {
+  TowerOfH(1, 3, 'A', 'B', 'C');
+  return 0;
 }
--- recursion/coutPath.cpp
+++ recursion/coutPath.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
+int countWaysToReachTop(int s,int e) {
+    // Base cases
+    if (s==e ) {
+        return 1;
+    } 
+     if (s>e) {
+        return 0;
+    }
 
-int findPath(int start , int end){
+    // Recursive cases
+    int ways = 0;
+    for (int i = 1; i <= 6; i++) {
+     
+        ways += countWaysToReachTop(s+i,e);
+      
+    }
 
-    if (start==end) return 1;
-    return 0+
-    findPath(start+1,end);
-
+    return ways;
 }
 
-int main(){
-cout << findPath(0,4);
+int main() {
+    int n;
+    cout << "Enter the number of steps: ";
+   cin >> n;
+
+    int ways = countWaysToReachTop(0,n);
+    cout << "Number of ways to reach the top: " << ways << endl;
+
     return 0;
 }
