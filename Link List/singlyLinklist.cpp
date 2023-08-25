19915ec3fd87a7b6b4166c3547d3d849f814cd8b
surajit <bikymaity489@gmail.com>
2023-08-25 11:29:53 +0530

    Link list reverse

--- Link List/singlyLinklist.cpp
+++ Link List/singlyLinklist.cpp
@@ -21,7 +21,6 @@ public:
 class LinkList
 {
 public:
-  int data;
   Node *Head;
   Node *Tail;
   LinkList()
@@ -113,7 +112,41 @@ public:
     }
     cout << "------" << endl;
   }
+
+  void reverse()
+  {
+    Node *prev = NULL;
+    Node *curr = this->Head;
+    Node *next;
+    while (curr != NULL)
+    {
+      next = curr->nextNode;
+      curr->nextNode = prev;
+      prev = curr;
+      curr = next;
+    }
+    this->Tail = this->Head;
+    this->Head = prev;
+  }
 };
+
+Node *recursiveRev(Node *head)
+{
+  if (head == NULL || head->nextNode == NULL)
+    return head;
+
+  Node *newHead = recursiveRev(head->nextNode);
+  head->nextNode->nextNode = head;
+  head->nextNode = NULL;
+  return newHead;
+}
+LinkList getRev(LinkList &list)
+{
+  LinkList rev;
+  list.Tail = list.Head;
+  list.Head = recursiveRev(list.Head);
+  return list;
+}
 
 int main()
 {
@@ -123,8 +156,13 @@ int main()
   node1.insertAtStart(8);
   node1.insertAt(11, 4);
   node1.insertAt(17, 6);
+  // node1.displayList();
+  // node1.reverse();
+  // node1.displayList();
+
+  node1.insertAt(45, 6);
   node1.displayList();
-  node1.deleteNode(-11);
+  getRev(node1);
   node1.displayList();
   return 0;
 }
